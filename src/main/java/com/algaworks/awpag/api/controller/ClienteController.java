35853ef0f4975f987c0fe67ca00b7ac7c81b4267
vpmapelli <viniciusmapelli@gmail.com>
2024-03-21 20:36:11 -0300

    Usando Jakarta Persistance

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package com.algaworks.awpag.api.controller;
 
 import com.algaworks.awpag.domain.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,22 +12,13 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("João");
-        cliente1.setTelefone("34 99999-11111");
-        cliente1.setEmail("joaodascouves@algaworks.com.br");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Maria");
-        cliente2.setTelefone("16 97777-1111");
-        cliente2.setEmail("mariadasilva@algaworks.com.br");
-
-        return Arrays.asList(cliente1, cliente2);
-
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 
 }
--- src/main/java/com/algaworks/awpag/domain/model/Cliente.java
+++ src/main/java/com/algaworks/awpag/domain/model/Cliente.java
@@ -1,15 +1,28 @@
 package com.algaworks.awpag.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+//Caso fosse necessário um mapeamento com o banco para uma tabela chamada tb_cliente
+//@Table(name = "tb_cliente")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @Column(name = "nome")
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
 }
